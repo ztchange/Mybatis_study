fb6b4bef25dec91ef19102e029a8ecd5cce5b717
ztzy <1477400312@qq.com>
2016-09-07 05:32:08 +0800

    新增config.xml Test.java User.java userMapper.xml

---
+++ MyBatis_Study/src/zt/zy/domain/Test.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package zt.zy.domain;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Test {
+	public static void main(String[] args) throws IOException {
+        //mybatis的配置文件
+        String resource = "config.xml";
+        //MyBatis 包含一个名叫 Resources 的工具类，它包含一些实用方法，可使从 classpath 或其他位置加载资源文件更加容易。
+        InputStream is=Resources.getResourceAsStream(resource);
+        //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+        //InputStream is = Test.class.getClassLoader().getResourceAsStream(resource);
+        
+        //构建sqlSession的工厂
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+        //Reader reader = Resources.getResourceAsReader(resource); 
+        //构建sqlSession的工厂
+        //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        //创建能执行映射文件中sql的sqlSession
+        SqlSession session = sessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "zt.zy.mapping.userMapper.getUser";//映射sql的标识字符串
+        //执行查询返回一个唯一user对象的sql
+        User user = session.selectOne(statement, 1);
+        System.out.println(user);
+    }
+
+}
